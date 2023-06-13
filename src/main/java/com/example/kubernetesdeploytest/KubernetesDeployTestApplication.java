807611d4207876df4e295ec4d5f8f7a8a9641352
korochev <myver33@gmail.com>
2023-06-13 20:06:38 +0300

    test

---
+++ src/main/java/com/example/kubernetesdeploytest/KubernetesDeployTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.kubernetesdeploytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesDeployTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesDeployTestApplication.class, args);
+    }
+
+}
